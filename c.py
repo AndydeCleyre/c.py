e30e2372aafc733139f12e367fca979a622596e7
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2015-07-23 17:08:03 +0200

    Introduce get_lexer()
    This patch introduces a method get_lexer() which returns a particular
    lexer class, depending on the filename and its content. Optionally the
    lexer can be forced with the keyword argument 'lexer'. The fallback
    lexer is TextLexer()

--- c.py
+++ c.py
@@ -20,19 +20,44 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters import Terminal256Formatter
 
 
-def determine_lexer(filename, data):
-    try:
-        lexer = guess_lexer_for_filename(filename, data)
-    except ClassNotFound:
-        # Check if there is any shebang.
-        if data[0:2] == '#!':
-            lexer = guess_lexer(data)
-        else:
-            lexer = TextLexer()
-    except TypeError:
-        lexer = TextLexer()
+def get_lexer(filename, data, lexer='auto'):
+    """
+    Return a particular lexer instance.
 
-    return lexer
+    This method wraps pygments' methods guess_lexer_for_filename()
+    and guess_lexer(). First guess_lexer_for_filename() is invoked;
+    if there is no result the presence of a shebang is checked.
+    If the subsequent call to guess_lexer() does not bring any
+    results the fallback value TextLexer() is returned.
+
+    Args:
+        filename        The name of the file to be displayed
+        data            The content of the file (utf-8 encoded)
+        lexer           Specifying another value than 'auto' skips any
+                        guess_lexer() calls. The pygments method
+                        get_lexer_by_name() is used to find a particular
+                        lexer class. If nothing has been found, c.py
+                        fails.
+    """
+    if lexer == 'auto':
+        try:
+            lexer_cls = guess_lexer_for_filename(filename, data)
+        except ClassNotFound:
+            # Check if there is any shebang.
+            if data[0:2] == '#!':
+                lexer_cls = guess_lexer(data)
+            else:
+                lexer_cls = TextLexer()
+        except TypeError:
+            lexer_cls = TextLexer()
+    else:
+        try:
+            lexer_cls = get_lexer_by_name(lexer)
+        except ClassNotFound:
+            print('Error: Invalid lexer!')
+            exit(1)
+
+    return lexer_cls
 
 
 def cli(args):
@@ -40,12 +65,8 @@ def cli(args):
     with open(filename) as f:
         data = f.read()
 
-    formatter = Terminal256Formatter(style='friendly', linenos=True)
-
-    if args['--lexer'] == 'auto':
-        lexer = determine_lexer(filename, data)
-    else:
-        lexer = get_lexer_by_name(args['--lexer'])
+    formatter = Terminal256Formatter(style='friendly')
+    lexer = get_lexer(filename, data, args['--lexer'])
 
     # Highlight! :-)
     out = highlight(data, lexer, formatter)
