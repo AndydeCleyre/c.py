AndydeCleyre/c.py

commit 00d453a45f1bef897c38e72e2107a9c4ba96c36f

Don't fail when there is binary input

c.py
@@ -59,7 +59,7 @@ def read_file(filename):
             return sys.stdin.read()
         else:
             debug("Reading file: '{}'".format(filename))
-            with open(filename) as f:
+            with open(filename, 'rb') as f:
                 return f.read()
     except Exception as e:
         echo(e, err=True)

commit 327438484662dc8999f7ffde35f3e169c6480b73

Fix regression in shebang detection
00d453a (Don't fail when there is binary input, 2015-09-30)
introduced a regression with shebang detection. Since the data
is read as bytes rather than UTF-8 the comparison does not match
any more. This patch fixes that problem. Additionally the data
has to be converted to UTF-8; otherwise guess_lexer() fails.

c.py
@@ -84,9 +84,9 @@ def get_lexer(filename, data, lexer='auto'):
         try:
             lexer_cls = guess_lexer_for_filename(filename, data)
         except ClassNotFound:
-            if data[0:2] == '#!':
+            if data[0:2] == b'#!':
                 debug("Shebang '{}' present".format(data.splitlines()[0]))
-                lexer_cls = guess_lexer(data)
+                lexer_cls = guess_lexer(data.decode())
             elif filename == '-':
                 try:
                     debug("Have read from 'stdin'; guessing lexer for content")